3652a08812304feb103ca3e20e928a34131bff29
Linortys <Karambos@yandex.ru>
2025-03-06 21:22:55 +0300

    Add files via upload

--- 2_hw.py
+++ 2_hw.py
@@ -7,3 +7,15 @@ def task_1(a: int, b: float, c: str, d: list, g: bool) -> str:
     print(g, 'Тип данных', type(g))
 
 task_1('1',7.3, 5, [5,5], True)
+
+def task_2() -> list:
+
+    a = [1, 2, 3, 5, 8, 13, 21]
+    print(a[0:3])   #числа Фибоначчи
+
+task_2()
+
+def task_3(a: int) -> int:
+    return a ** 2
+
+print(task_3(4))
