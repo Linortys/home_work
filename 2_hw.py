6ca21d3ca17676c48d15334a4d9292b6def7c31d
Linortys <Karambos@yandex.ru>
2025-03-06 21:01:02 +0300

    Add files via upload

---
+++ 2_hw.py
@@ -0,0 +1,9 @@
+def task_1(a: int, b: float, c: str, d: list, g: bool) -> str:
+
+    print (a, 'Тип данных', type(a))
+    print (b, 'Тип данных', type(b))
+    print(c, 'Тип данных', type(c))
+    print(d, 'Тип данных', type(d))
+    print(g, 'Тип данных', type(g))
+
+task_1('1',7.3, 5, [5,5], True)
