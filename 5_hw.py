251150756478b6a5f542844fc77fac8194c9a1ba
Linortys <Karambos@yandex.ru>
2025-03-16 14:37:47 +0300

    Add files via upload

---
+++ 5_hw.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+
+username_find = driver.find_element(By.ID, 'user-name')
+pass_find = driver.find_element(By.ID, 'password')
+submit_find = driver.find_element(By.ID, 'login-button')
+
+
+if submit_find is None:
+    print("Один из элементов не найден")
+elif username_find is None:
+    print("Один из элементов не найден")
+elif pass_find is None:
+    print("Один из элементов не найден")
+else:
+    print('Элементы найдены')
