c2dbcaf6069a6cf774feba15efb66e5fe7570bff
Linortys <Karambos@yandex.ru>
2025-03-20 18:44:43 +0300

    Add files via upload

--- 5_hw.py
+++ 5_hw.py
@@ -5,19 +5,17 @@ driver = webdriver.Chrome()
 
 def open_url():
     driver.get("https://www.saucedemo.com/")
+    a = driver.find_element(By.ID, 'user-name')
+    b = driver.find_element(By.ID, 'password')
+    c = driver.find_element(By.ID, 'login-button')
 
+    if a is None:
+        print("Один из элементов не найден")
+    elif b is None:
+        print("Один из элементов не найден")
+    elif c is None:
+        print("Один из элементов не найден")
+    else:
+        print('Элементы найдены')
 
 open_url()
-username_find = driver.find_element(By.ID, 'user-name')
-pass_find = driver.find_element(By.ID, 'password')
-submit_find = driver.find_element(By.ID, 'login-button')
-
-
-if submit_find is None:
-    print("Один из элементов не найден")
-elif username_find is None:
-    print("Один из элементов не найден")
-elif pass_find is None:
-    print("Один из элементов не найден")
-else:
-    print('Элементы найдены')
